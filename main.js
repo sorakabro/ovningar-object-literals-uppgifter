2e18eb393cb27d3ecdb52e4d304d8ca7e322b261
Benjamin Oldén <berntsson90@hotmail.com>
2020-11-24 11:43:52 +0100

    object ovning uppgift 4

---
+++ main.js
@@ -0,0 +1,88 @@
+
+
+// Skapa skolobjekt
+
+let medieinstitutet = {
+    name: "Medieinstitutet",
+    students: [],
+    teachers: [],
+    address: "Malmö vägen 123",
+    zipCode: "123321",
+    city: "Malmö"
+}
+
+
+// Skapa tre stycken olika ämnen inneha name, students, teacher
+
+let matematik = {
+    name: "Matematik",
+    students: [],
+    teacher: []
+}
+
+
+let svenska = {
+    name: "Svenska",
+    students: [],
+    teacher: []
+}
+
+
+let engelska = {
+    name: "Engelska",
+    students: [],
+    teacher: []
+}
+
+// Skapa fem stycken studenter, egenskaper name, age,gender och subjects
+
+let olle = {
+    name: "Olle",
+    age: 32,
+    gender: "Male",
+    subjects: []
+}
+
+let karin = {
+    name: "Karin",
+    age: 30,
+    gender: "Female",
+    subjects: []
+}
+
+let kalle = {
+    name: "Kalle",
+    age: 25,
+    gender: "Male",
+    subjects: []
+}
+
+
+let maria = {
+    name: "Maria",
+    age: 22,
+    gender: "Female",
+    subjects: []
+}
+
+let alex = {
+    name: "Alex",
+    age: 48,
+    gender: "Male",
+    subjects: []
+}
+
+// Skapa två lärare, egenskaper name och subjects
+
+let peter = {
+    name: "Peter",
+    subjects: []
+
+}
+
+let niklas = {
+    name: "Niklas",
+    subjects: []
+
+}
+console.log(peter, niklas);
